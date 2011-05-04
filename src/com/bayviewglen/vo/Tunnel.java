1493ddbefdec01630e223f1b66cf637fc292dd10
Test Account <kevindeslauriers@gmail.com>
2011-05-04 15:53:48 -0400

    Added Game class.  Removed GameVO from GameGUI.  Originally completed by Boris
    Signed-off-by: Test Account <kevindeslauriers@gmail.com>

--- src/com/bayviewglen/vo/Tunnel.java
+++ src/com/bayviewglen/vo/Tunnel.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 public class Tunnel implements Serializable{
 	// This will have two exits that are building.  Each Building has a room that contains a tunnel
 	// entrance.  This is the room that the player will be entering from a tunnel.
-
+	
 	
 	// Tunnels may be joined together via other tunnels.  I have a few ideas about
 	// this but for now I am leaving this undecided.
