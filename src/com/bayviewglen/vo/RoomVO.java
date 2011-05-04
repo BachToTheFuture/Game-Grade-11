61e5933e1df8e000c5a2cedd6610ca462be27853
Boris Ivanovic <bivanovic@bayviewglen.ca>
2011-05-03 20:33:12 -0400

    Added Listeners for Menus to Game class. The Game class now has Listeners for Mouse Events, Key Events, and Menu Events.
    Signed-off-by: Boris Ivanovic <bivanovic@bayviewglen.ca>

--- src/com/bayviewglen/vo/RoomVO.java
+++
@@ -1,8 +0,0 @@
-
-package com.bayviewglen.vo;
-
-import java.io.Serializable;
-
-public class RoomVO implements Serializable{
-  int i=0;
-}
--- src/com/bayviewglen/windows/Game.java
+++ src/com/bayviewglen/windows/Game.java
@@ -8,6 +8,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -18,6 +21,8 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.event.MenuKeyEvent;
+import javax.swing.event.MenuKeyListener;
 
 import com.bayviewglen.vo.Town;
 
@@ -25,12 +30,10 @@ import com.bayviewglen.vo.Town;
  * @author kdeslauriers
  *
  */
-public class Game {
+public class Game implements ActionListener, KeyListener, MouseListener{
 
 	private ArrayList<Town> villages;
-	private ListenerForKeys lfk = new ListenerForKeys();
-	private ListenerForMouse lfm = new ListenerForMouse();
-	
+
 	/**
 	 * @param args
 	 */
@@ -41,111 +44,115 @@ public class Game {
 
 	private Game(){
 		JFrame frame = new JFrame("Game Title");
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize().getSize();
 		frame.setSize(dim.width, dim.height);
-		
-		initGameMenu(frame);
-		
-		frame.addKeyListener(lfk);
-		frame.addMouseListener(lfm);
+
+		initGameMenu(frame, this);
+
+		frame.addKeyListener(this);
+		frame.addMouseListener(this);
 		frame.setVisible(true);
 
 	}
-	
-	public static void initGameMenu(JFrame frame){
+
+	public static void initGameMenu(JFrame frame, Game mainGame){
 		JMenuBar menuBar;
 		JMenu game, gameplay, help;
 		JMenuItem menuItem;
-		
+
 		menuBar = new JMenuBar();
-		
+
 		/* Basic Game */
 		game = new JMenu("Game");
 		menuBar.add(game);
-		
+
 		menuItem = new JMenuItem("New Game");
+		menuItem.addActionListener(mainGame);
 		game.add(menuItem);
 		game.addSeparator();
 		menuItem = new JMenuItem("Load");
+		menuItem.addActionListener(mainGame);
 		game.add(menuItem);
 		menuItem = new JMenuItem("Save");
+		menuItem.addActionListener(mainGame);
 		game.add(menuItem);
 		game.addSeparator();
 		menuItem = new JMenuItem("Exit");
+		menuItem.addActionListener(mainGame);
 		game.add(menuItem);
-		
-		
+
+
 		/* Game Play */
 		gameplay = new JMenu("Commands");
 		menuBar.add(gameplay);
-		
+
 		menuItem = new JMenuItem("Inventory/Statistics");
+		menuItem.addActionListener(mainGame);
 		gameplay.add(menuItem);
 		menuItem = new JMenuItem("View Map");
+		menuItem.addActionListener(mainGame);
 		gameplay.add(menuItem);
 		menuItem = new JMenuItem("Trade");
+		menuItem.addActionListener(mainGame);
 		gameplay.add(menuItem);
 		menuItem = new JMenuItem("Open Door/Chest");
+		menuItem.addActionListener(mainGame);
 		gameplay.add(menuItem);
-		
+
 		help = new JMenu("Help");
 		menuBar.add(help);
-		
+
 		/*Help Menu */
 		menuItem = new JMenuItem("Game Help");
+		menuItem.addActionListener(mainGame);
 		help.add(menuItem);
 		menuItem = new JMenuItem("About the Game");
+		menuItem.addActionListener(mainGame);
 		help.add(menuItem);
-		
+
 		frame.setJMenuBar(menuBar);
 	}
-	
-	private class ListenerForKeys implements KeyListener
+
+	public void keyPressed(KeyEvent e)
 	{
-		public void keyPressed(KeyEvent e)
-		{
-			System.out.println("Key Pressed: " + e.getKeyCode());
-			System.out.println(e.getModifiersEx());
-			System.out.println(e.isShiftDown());
-		}
-
-		public void keyReleased(KeyEvent e)
-		{
-
-		}
-
-		public void keyTyped (KeyEvent e)
-		{
-
-		}
-
+		System.out.println("Key Pressed: " + e.getKeyCode());
+		System.out.println(e.getModifiersEx());
+		System.out.println(e.isShiftDown());
 	}
-	
-	private class ListenerForMouse implements MouseListener
+
+	public void keyReleased(KeyEvent e)
 	{
 
-		public void mouseClicked(MouseEvent e) {
-			System.out.println("Mouse was pressed on " + e.getComponent().getName());
-		}
+	}
 
-		public void mousePressed(MouseEvent e) {
-			
-		}
-
-		public void mouseReleased(MouseEvent e) {
-			
-		}
-
-		public void mouseEntered(MouseEvent e) {
-			
-		}
-
-		public void mouseExited(MouseEvent e) {
-			
-		}
+	public void keyTyped (KeyEvent e)
+	{
 
 	}
-	
+
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("Mouse was pressed on " + e.getComponent().getName());
+	}
+
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Mouse was pressed on " + e.getActionCommand());
+	}
 }
